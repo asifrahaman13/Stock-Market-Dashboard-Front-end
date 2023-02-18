e06926ace2d089c8524bab8c4be9e591070a6db7
asifrahaman13 <asifrahaman162@gmail.com>
2023-02-18 14:46:33 +0530

    another commit

--- src/components/Right/Charts/LIneChart/LineChart.jsx
+++ src/components/Right/Charts/LIneChart/LineChart.jsx
@@ -1,6 +1,7 @@
 // ./components/LineChart.js
 
 import React, { useState, useEffect } from "react";
+import Chart from "chart.js/auto";
 import { Line } from "react-chartjs-2";
 import "./LineChart.css";
 
